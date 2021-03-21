2c4dea3b6b975054cce61162ef6d1707558b9c89
Nuno Oliveira <ctw00148@criticaltechworks.com>
2021-03-21 23:10:23 +0000

    chore: added main class and application.properties file

---
+++ src/main/java/com/nj/njplaygroundspring/NjPlaygroundSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nj.njplaygroundspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjPlaygroundSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjPlaygroundSpringApplication.class, args);
+	}
+
+}
